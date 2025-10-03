7ff59de43d95d93457d117299348584181636cef
Abdullah Kise <abdullahkise@hotmail.com>
2025-10-03 16:38:06 +0300

    gun3

---
+++ Gun3/kutuphanem/nesnelerim.py
@@ -0,0 +1,11 @@
+#notebook özel komutudur. bu içeriğini belirten yolda bir dosyaya yazmak.
+#dosya/klasor yolunu $ ile değişkenden alabiliriz.
+def c2f(c:int)->float:
+    """
+        int tipinden C alır. F'yi hesaplar
+    """
+    return c*1.8 + 32
+
+#--yeni değişkenler--
+yazar = "Abdullah Kise"
+mail = "abdullahkise@hotmail.com"
